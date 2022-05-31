f32aa5c0c5df46683502daf3d077bba816f944f2
AntonyDin <antdinyakov@gmail.com>
2022-05-31 12:59:13 +0500

    fixed hide

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,7 +37,7 @@ class QuestionsController < ApplicationController
   def hide
     @question.update(hidden: true)
 
-    redirect_to question_path
+    redirect_to question_path(@question)
   end
 
   private
